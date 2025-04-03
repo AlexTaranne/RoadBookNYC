f89d4d9e122022dff4dfa3966ac8a5e85bd3b2f7
Alex Taranne <alextaranne@gmail.com>
2025-04-03 17:00:20 +0200

    fix css

--- src/pages/Leon.tsx
+++ src/pages/Leon.tsx
@@ -1,10 +1,12 @@
 export default function Leon() {
-    return (
-        <>
-             <img src="/lapin.png" alt="" className="pic-profil"/>
-            <p>Prénom: Léon</p>
-            <p>Surnom: Lapin</p>
-            <p>Rôle: Fils</p>
-            <p>Note: Peut mettre un p'tit dessin animé?</p>
-        </>
-        )}
+  return (
+    <div className="profil">
+      {" "}
+      <img src="/lapin.png" alt="" className="pic-profil" />
+      <p>Prénom: Léon</p>
+      <p>Surnom: Lapin</p>
+      <p>Rôle: Fils</p>
+      <p>Note: Peut mettre un p'tit dessin animé?</p>
+    </div>
+  );
+}
